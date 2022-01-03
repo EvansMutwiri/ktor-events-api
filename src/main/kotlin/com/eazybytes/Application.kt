3bfc219541f7f2f144f0f28954f60c7ca090b44f
EvansMutwiri <mutwirievansm@gmail.com>
2022-01-04 02:00:24 +0300

    Get events endpoint

--- src/main/kotlin/com/eazybytes/Application.kt
+++ src/main/kotlin/com/eazybytes/Application.kt
@@ -1,56 +1,46 @@
 package com.eazybytes
 
-import com.eazybytes.entities.EventEntity
-import com.eazybytes.entities.EventEntity.id
+import com.eazybytes.plugins.configureRouting
+import com.eazybytes.plugins.configureSerialization
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.eazybytes.plugins.*
-import org.ktorm.database.Database
-import org.ktorm.dsl.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
 
-        val database = Database.connect(
-            url = "jdbc:mysql://localhost:3306/simpledb",
-            driver = "com.mysql.cj.jdbc.Driver",
-            user = "eazzy",
-            password = "*******"
-        )
-
-        //delete
-        database.delete(EventEntity) {
-            it.id eq 1
-        }
-
-        //edit or update
-        database.update(EventEntity) {
-            set(it.name, "engineer")
-            set(it.price, "3000")
-            set(it.description, "trade fair")
-
-            where {
-                it.id eq 2
-            }
-        }
-
-        //insert
-        database.insert(EventEntity) {
-            set(it.id, 1)
-            set(it.name, "myevent")
-            set(it.poster, "poster.link")
-            set(it.description, "my event is a sample event")
-            set(it.price, "2000")
-
-        }
-
-        //read from db
-        var event = database.from(EventEntity).select()
-
-        for (row in event) {
-            println("${row[EventEntity.id]}: ${row[EventEntity.name]}, ${row[EventEntity.description]}")
-        }
+//        //delete
+//        database.delete(EventEntity) {
+//            it.id eq 1
+//        }
+//
+//        //edit or update
+//        database.update(EventEntity) {
+//            set(it.name, "engineer")
+//            set(it.price, "3000")
+//            set(it.description, "trade fair")
+//
+//            where {
+//                it.id eq 2
+//            }
+//        }
+//
+//        //insert
+//        database.insert(EventEntity) {
+//            set(it.id, 1)
+//            set(it.name, "myevent")
+//            set(it.poster, "poster.link")
+//            set(it.description, "my event is a sample event")
+//            set(it.price, "2000")
+//
+//        }
+//
+//        //read from db
+//        var event = database.from(EventEntity).select()
+//
+//        for (row in event) {
+//            println("${row[EventEntity.id]}: ${row[EventEntity.name]}, ${row[EventEntity.description]}")
+//        }
     }.start(wait = true)
 }
--- src/main/kotlin/com/eazybytes/plugins/Routing.kt
+++ src/main/kotlin/com/eazybytes/plugins/Routing.kt
@@ -1,58 +1,42 @@
 package com.eazybytes.plugins
 
-import com.eazybytes.models.UserInfo
-import com.eazybytes.models.UserResponse
+import com.eazybytes.entities.EventEntity
+import com.eazybytes.entities.db.DatabaseConnection
+import com.eazybytes.models.EventItem
 import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import java.io.File
+import org.ktorm.dsl.from
+import org.ktorm.dsl.map
+import org.ktorm.dsl.select
 
 fun Application.configureRouting() {
 
+    val db = DatabaseConnection.database
+
     routing {
-
-        get("/headers") {
-            call.response.headers.append("server", "ktor_server", safeOnly = true)
-            call.respond("Headers attached")
-        }
-        get("/") {
-            val userResponse = UserResponse(username = "evans")
-            call.respond(userResponse)
-        }
-
-        post("/login") {
-            val userInfo = call.receive<UserInfo>()
-            println(userInfo)
-            call.respond(userInfo.username)
-        }
         get("/events") {
-//            val eventItem = EventItem
-            call.respond(mapOf("poster" to "this is a poster", "name" to "mr & ms isiolo"))
-        }
+//            call.respondText("return all events")
 
-        get("/download") {
-            val file = File("src/imagess/shoppe.png")
-            call.response.header(
-                HttpHeaders.ContentDisposition,
-                ContentDisposition.Attachment.withParameter(
-                    ContentDisposition.Parameters.FileName, "TermsConditions.png"
-                ).toString()
-            )
-            call.respondText("File downloading...")
-            call.respondFile(file)
-        }
+            /**
+             * @Serializable
+             * add dependency
+             */
 
-        get("/openImage") {
-            val file = File("src/imagess/shoppe.png")
-            call.response.header(
-                HttpHeaders.ContentDisposition,
-                ContentDisposition.Inline.withParameter(
-                    ContentDisposition.Parameters.FileName, "TermsConditions.png"
-                ).toString()
-            )
-            call.respondFile(file)
+            val events = db.from(EventEntity).select().map {
+                val id = it[EventEntity.id]
+                val name = it[EventEntity.name]
+                val description = it[EventEntity.description]
+                val poster = it[EventEntity.poster]
+                val price = it[EventEntity.price]
+                EventItem(
+                    id ?: -1,
+                    description ?: "",
+                    name ?: "",
+                    poster ?: "",
+                    price ?: "")
+            }
+            call.respond(events)
         }
     }
 }
